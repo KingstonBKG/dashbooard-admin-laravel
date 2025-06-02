02691af3baad2dcb78d9cfd1349993b5088716f4
Stesie123 <janpoustesie@gmail.com>
2025-06-02 14:58:32 +0100

    creation de la modale details de tontine

--- resources/views/pages/dashboard/analytics/dashboard-analytics.blade.php
+++ resources/views/pages/dashboard/analytics/dashboard-analytics.blade.php
@@ -21,7 +21,7 @@
       <div class="d-flex align-items-start row">
         <div class="col-sm-7">
           <div class="card-body">
-            <h5 class="card-title text-primary mb-3">Congratulations Bako! 🎉</h5>
+            <h5 class="card-title text-primary mb-3">Congratulations {{ Auth::user()->username }}! 🎉</h5>
             <p class="mb-6">You have done 72% more sales today.<br>Check your new badge in your profile.</p>
 
             <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
